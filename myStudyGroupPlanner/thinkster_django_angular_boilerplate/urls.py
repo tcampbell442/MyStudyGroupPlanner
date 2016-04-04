8a7ca7ef04109c366c09cc61ec73190d0f2dddf2
soup <t.campbell442@gmail.com>
2016-04-04 19:19:35 -0400

    added login functionality

--- myStudyGroupPlanner/thinkster_django_angular_boilerplate/urls.py
+++ myStudyGroupPlanner/thinkster_django_angular_boilerplate/urls.py
@@ -4,7 +4,7 @@ from thinkster_django_angular_boilerplate.views import IndexView
 
 from rest_framework_nested import routers
 
-from authentication.views import AccountViewSet
+from authentication.views import AccountViewSet, LoginView
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
@@ -13,6 +13,6 @@ urlpatterns = patterns(
      '',
     # ... URLs
     url(r'^api/v1/', include(router.urls)),
-
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url('^.*$', IndexView.as_view(), name='index'),
 )
