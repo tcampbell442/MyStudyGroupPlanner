b71bebfa1d8cc9acda4496389f9252534cf84ddf
soup <t.campbell442@gmail.com>
2016-04-04 19:58:33 -0400

    added non-commited file

---
+++ myStudyGroupPlanner/static/javascripts/myStudyGroupPlanner.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('myStudyGroupPlanner', [
+      'myStudyGroupPlanner.routes',
+      'myStudyGroupPlanner.authentication',
+      'myStudyGroupPlanner.config',
+      'myStudyGroupPlanner.layout'
+    ]);
+
+  angular
+    .module('myStudyGroupPlanner.config', []);	
+
+  angular
+    .module('myStudyGroupPlanner.routes', ['ngRoute']);
+
+  angular
+  .module('myStudyGroupPlanner')
+  .run(run);
+
+run.$inject = ['$http'];
+
+/**
+* @name run
+* @desc Update xsrf $http headers to align with Django's defaults
+*/
+function run($http) {
+  $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $http.defaults.xsrfCookieName = 'csrftoken';
+}
+})();
---
+++ myStudyGroupPlanner/static/javascripts/myStudyGroupPlanner.routes.js
@@ -0,0 +1,41 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('myStudyGroupPlanner.routes')
+    .config(config);
+
+  config.$inject = ['$routeProvider'];
+
+  /**
+  * @name config
+  * @desc Define valid application routes
+  */
+  function config($routeProvider) {
+    $routeProvider.when('/register', {
+      controller: 'RegisterController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/authentication/register.html'
+    }).when('/login', {
+  controller: 'LoginController',
+  controllerAs: 'vm',
+  templateUrl: '/static/templates/authentication/login.html'
+}).when('/home', {
+      controller: 'HomeController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/home.html'
+    }).when('/groupMember',{
+      controller: 'groupMemberController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/groupMember.html'
+    }).when('/modal',{
+      controller: "modalController",
+      controllerAs:"vm",
+      templateUrl:'/static/templates/modal.html'
+    }).when('/adminHome', {
+      controller: 'AdminHomeController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/adminHome.html'
+    }).otherwise('/');
+  }
+})();
