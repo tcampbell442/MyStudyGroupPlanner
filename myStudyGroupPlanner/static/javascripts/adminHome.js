dd592dc46ae5c0000560cd483917e712085a9086
Aparna <aparnak1@umbc.edu>
2016-04-04 19:18:51 -0400

    Partially updated admin files.

--- myStudyGroupPlanner/static/javascripts/adminHome.js
+++ myStudyGroupPlanner/static/javascripts/adminHome.js
@@ -1,26 +1,50 @@
 (function(){
-  'use strict';
-
-  angular
-    .module('thinkster')
-    .controller('AdminHomeController', AdminHomeController);
-
-  AdminHomeController.$inject = ['$location', '$scope', 'Authentication'];
-
-  /**
-  * @namespace AdminHomeController
-  */
-  function AdminHomeController($location, $scope, Authentication) {
-    var vm = this;
-
-    vm.tab = 1;
-
-    vm.selectTab = function(setTab){
-      vm.tab = setTab;
+    'use strict';
+    
+    angular
+	.module('thinkster')
+	.controller('AdminHomeController', AdminHomeController);
+    
+    AdminHomeController.$inject = ['$location', '$scope', 'Authentication'];
+    
+    /**
+     * @namespace AdminHomeController
+     */
+    function AdminHomeController($location, $scope, Authentication) {
+	var vm = this;
+	
+	vm.tab = 1;
+      
+	vm.reports = [
+	    {
+		code: "111",
+		date: "mm/dd/yyyy",
+	    },
+	    
+	    {
+		code: "222",
+		date: "mm/dd/yyyy",
+	    },
+	    
+	    {
+		code: "333",
+		date: "mm/dd/yyyy",
+	    },
+	    
+	    {
+		code: "444",
+		date: "mm/dd/yyyy",
+	    }];
+	
+	vm.selectTab = function(setTab){
+	    vm.tab = setTab;
+	}
+	
+	vm.isSelected = function(checkTab){
+	    return vm.tab === checkTab;
+	}
+	
+	vm.rooms = {};
+	vm.users = {};
     }
-
-    vm.isSelected = function(checkTab){
-      return vm.tab === checkTab;
-    }
-  }
 })();
