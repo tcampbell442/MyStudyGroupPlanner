bd93fe94d698bb6c079b09d225dc62e0e06a749f
Siqi Lin <linsiqi1@gmail.com>
2016-04-06 23:25:38 -0400

    added input date to modal

--- myStudyGroupPlanner/static/javascripts/modal.js
+++ myStudyGroupPlanner/static/javascripts/modal.js
@@ -1,50 +1,16 @@
-var mymodal = angular.module('mymodal', []);
+(function(){
+    'use strict';
 
-mymodal.controller('modalController', function ($scope) {
-    $scope.showModal = false;
-    $scope.toggleModal = function(){
-        $scope.showModal = !$scope.showModal;
-    };
-  });
+    angular
+	.module('myStudyGroupPlanner')
+	.controller('modalController', modalController);
 
-mymodal.directive('modal', function () {
-    return {
-      template: '<div class="modal fade">' +
-          '<div class="modal-dialog">' +
-            '<div class="modal-content">' +
-              '<div class="modal-header">' +
-                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-                '<h4 class="modal-title">{{ title }}</h4>' +
-              '</div>' +
-              '<div class="modal-body" ng-transclude></div>' +
-            '</div>' +
-          '</div>' +
-        '</div>',
-      restrict: 'E',
-      transclude: true,
-      replace:true,
-      scope:true,
-      link: function postLink(scope, element, attrs) {
-        scope.title = attrs.title;
+    modalController.$inject = ['$location', '$scope', 'Authentication'];
 
-        scope.$watch(attrs.visible, function(value){
-          if(value == true)
-            $(element).modal('show');
-          else
-            $(element).modal('hide');
-        });
-
-        $(element).on('shown.bs.modal', function(){
-          scope.$apply(function(){
-            scope.$parent[attrs.visible] = true;
-          });
-        });
-
-        $(element).on('hidden.bs.modal', function(){
-          scope.$apply(function(){
-            scope.$parent[attrs.visible] = false;
-          });
-        });
-      }
-    };
-  });
+    /**
+     * @namespace modalController
+     */
+    function modalController($location, $scope, Authentication) {
+	     var vm = this;
+     }
+})();
