7e1a9482a6249bfc70afbb4e92214386d01411dc
Siqi Lin <linsiqi1@gmail.com>
2016-04-13 20:41:31 -0400

    fixed the indentation error on views.py

--- myStudyGroupPlanner/report/views.py
+++ myStudyGroupPlanner/report/views.py
@@ -10,46 +10,10 @@ from models import Report
 class ReportList(generics.ListCreateAPIView):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
-    @api_view(['GET', 'POST'])
-    def reportRequest(request):
-	"""
-	List all Reports or create a new report
-	"""
-	if request.method =='GET':
-		reports = Report.objects.all()
-		serializer = ReportSerializer(reports, many=Ture)
-		return Response(serializer.data)
-
-	elif request.method =='POST':
-		serializer = ReportSerializer(data=request.DATA)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status.HTTP_201_CREATED)
-		else:
-			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ReportDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
 
-	@api_view(['GET', 'PUT', 'DELETE'])
-    def groupRequest(request, i):
-
-	report = Report.objects.get(i)
-
-	if request.method == 'GET':
-		serializer = ReportSerializer(report)
-		return Response(serializer.data)
-
-	elif request.method == 'PUT':
-		serializer = ReportSerializer(report, data=request.DATA)
-		if serializer.is_valid():
-		    serializer.save()
-		    return Response(serializer.data)
-		else:
-		    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-	elif request.method == 'DELETE':
-		report.delete()
-	    	return Response(status=status.HTTP_204_NO_CONTENT)
+	
