2717506fea12bfa670fadc101e202584f2aaf1ec
Siqi Lin <linsiqi1@gmail.com>
2016-04-26 19:21:11 -0400

    adding study location fixed

--- myStudyGroupPlanner/Class/models.py
+++ myStudyGroupPlanner/Class/models.py
@@ -6,15 +6,15 @@ class Subject(models.Model):
 	def __unicode__(self):
 		return self.subject
 
-class Class(models.Model):
-	subject = models.ForeignKey(Subject, related_name="subjects", null=True)	
+class SubjectsClass(models.Model):
+	subject = models.ForeignKey(Subject, related_name="subjectsClass", null=True)
 	subjectsClass = models.CharField(max_length=32)
 
 	def __unicode__(self):
 		return self.subjectsClass
 
 class Section(models.Model):
-	subjectsClass = models.ForeignKey(Class, related_name="Classes", null=True)	
+	subjectsClass = models.ForeignKey(SubjectsClass, related_name="section", null=True)
 	section = models.IntegerField()
 
 	def __unicode__(self):
--- myStudyGroupPlanner/Class/serializers.py
+++ myStudyGroupPlanner/Class/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from Class.models import Subject, Class, Section
+from Class.models import Subject, SubjectsClass, Section
 
 class SectionSerializer(serializers.ModelSerializer):
 	class Meta:
@@ -10,12 +10,12 @@ class ClassSerializer(serializers.ModelSerializer):
 	section = SectionSerializer(many=True, read_only=True)
 
 	class Meta:
-		model = Class
+		model = SubjectsClass
 		field = ('id', 'subjectsClass', 'section')
 
 class SubjectSerializer(serializers.ModelSerializer):
-	Class = ClassSerializer(many=True, read_only=True)
+	subjectsClass = ClassSerializer(many=True, read_only=True)
 
 	class Meta:
 		model = Subject
-		field = ('id', 'subject', 'Class')
+		field = ('id', 'subject', 'subjectsClass')
